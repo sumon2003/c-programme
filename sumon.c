66b9b8e57ca6327974854ff114070a2d4dbffbaa
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-13 19:17:54 +0600

    udated

--- sumon.c
+++ sumon.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    printf("SDL is a leading \nsoftware company in BD.");
+    printf("SDL is a leading \nsoftware company in BD.\n");
+    printf("sumon khan will be the best programmer in the world.");
+
     return 0;
 }
