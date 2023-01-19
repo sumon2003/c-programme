eae23e5eb17840e3e957455838ea9a3aae480dda
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-19 14:39:45 +0600

    Updated2

--- sumon.c
+++ sumon.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
 int main()
 {
-     printf("This is Md Sumon Hossain Khan.\n I'm 18 years old.\n I'm learning C.\n\n");
-     printf("This is my first programme.");
+    //This is comment.
+    /*I'm new in C programming.
+    So it could be wrong.*/
+     printf("This is Md Sumon Hossain Khan.\\ I'm 18 years old.\n I'm learning C.\n\n");
+     printf("This is my first programme.\n"); //This is a comment.
+     printf("C programming is very easy.\tI love it to learn.\n");
+     printf("They call him \"Sumon\"");
     return 0;
 }
