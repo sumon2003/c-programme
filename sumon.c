efaa3199ed30f56f1941d5b0a61337420ee9384c
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-19 17:22:41 +0600

    Updared3

--- sumon.c
+++ sumon.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    //This is comment.
+    //This is  a single comment.
     /*I'm new in C programming.
-    So it could be wrong.*/
+    So it could be wrong.
      printf("This is Md Sumon Hossain Khan.\\ I'm 18 years old.\n I'm learning C.\n\n");
      printf("This is my first programme.\n"); //This is a comment.
      printf("C programming is very easy.\tI love it to learn.\n");
-     printf("They call him \"Sumon\"");
+     printf("They call him \"Sumon\"");*/
+     int myNum = 15;
+     float MyFloatNum = 19.99;
+     char MyLetter = 'S';
+
+     printf("%d\n", myNum);
+     printf("%f\n", MyFloatNum);
+     printf("%c\n", MyLetter);
+
     return 0;
 }
