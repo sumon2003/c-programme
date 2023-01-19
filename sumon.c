b1c2f9ea8091a3d1f7358f29d73fccfdbfcf9dd9
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-19 14:20:06 +0600

    Updated1

--- sumon.c
+++ sumon.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main()
 {
-     printf("This is Md Sumon Hossain Khan.\n");
-     printf("I'm 18 years old.")
+     printf("This is Md Sumon Hossain Khan.\n I'm 18 years old.\n I'm learning C.\n\n");
+     printf("This is my first programme.");
     return 0;
 }
