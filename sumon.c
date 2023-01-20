b7db4bc97393c5dc75132837ffceaf19cf9ff109
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-20 07:39:51 +0600

    Updated5

--- sumon.c
+++ sumon.c
@@ -7,7 +7,7 @@ int main()
      printf("This is Md Sumon Hossain Khan.\\ I'm 18 years old.\n I'm learning C.\n\n");
      printf("This is my first programme.\n"); //This is a comment.
      printf("C programming is very easy.\tI love it to learn.\n");
-     printf("They call him \"Sumon\"");*/
+     printf("They call him \"Sumon\"");
      int myNum = 1000;
      int myOtherNum;
      myOtherNum = myNum;
@@ -17,9 +17,29 @@ int main()
      int x, y, z;
      x = y = z = 50;
      
-     printf("%d", x + y + z);
+     printf("%d\n", x + y + z);
+
+     float sumon = 19.99;
+     double sumi = 15.55;
+
+     printf("%f\n", sumon);
+     printf("%lf\n", sumi);
+
+     float myFloat = 5.5;
+
+     printf("%f\n", myFloat);
+     printf("%.1f\n", myFloat);
+     printf("%.2f\n", myFloat);
+     printf("%.4f\n", myFloat); */
+
+     int x = 5;
+     int y = 2;
+     int sum = 5 / 2;
+
+     printf("%d\n", sum);
+
+
      
-    
 
     return 0;
 }
