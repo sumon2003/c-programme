181c3e90866a90b77e17e539e71c1db7ea929c44
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-22 07:49:37 +0600

    Updated11

--- sumon.c
+++ sumon.c
@@ -139,6 +139,29 @@ int main()
     } else {
       printf("This is 0.\n");
     }
-
+    
+    int day = 7;
+    switch(day){
+      case 1:
+      printf("Friday\n");
+      break;
+      case 2:
+      printf("Tuesday\n");
+      break;
+      case 3:
+      printf("Saturday\n");
+      break;
+      case 4:
+      printf("Monday\n");
+      break;
+      case 5:
+      printf("Wednesday\n");
+      break;
+      case 6:
+      printf("Sunday\n");
+      break;
+      case 7:
+      printf("Thursday\n");
+    }
      return 0;
 }
