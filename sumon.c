9982e0f2c01ffa975ffb22b2e0a13d9d47dc2a07
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-24 18:49:28 +0600

    Update23

--- sumon.c
+++ sumon.c
@@ -291,6 +291,13 @@ int main()
       printf("%d\n", strlen(alphabet));
       printf("%d\n", sizeof(alphabet));
 
+      char str1[20] = "Hello ";
+      char str2[] = "world";
+      // Concatenate str2 to str1 (the result is stored in str1)
+      strcat(str1, str2);
+      // Print str1
+      printf("%s", str1);
+
 
     
      return 0;
