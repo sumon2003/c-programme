e0c01ed57c6f590510ba25eb30799c25735a71f2
sumon2003 <sumonkhanbd2003@gmail.com>
2023-01-25 09:48:47 +0600

    Update28

--- sumon.c
+++ sumon.c
@@ -344,6 +344,19 @@ int main()
       //Print the character
       printf("Your character is: %c\n", character);
 
+      // Name: Take string input
+      //Create a string
+      char firstName[30];
+
+      // Ask the user to input some text(Name)
+      printf("Enter your first name and press enter: \n");
+
+      // Get and save the text
+      scanf("%s,", firstName);
+
+      // Output the text
+      printf("Hello %s\n", firstName);
+
     
      return 0;
 }
