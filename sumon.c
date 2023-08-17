1a119a87548966f3f7efae85b2725ef8d8182b40
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-17 07:23:05 +0600

    Absolute_value

--- sumon.c
+++ sumon.c
@@ -1,13 +1,8 @@
 #include<stdio.h> 
 #include<math.h>
 int main(){
-	int A = 50;
-	int B = 100;
-	int temp;
-	temp = A;
-	A = B;
-	B = temp;
-	printf("A and B value are: %d %d\n", A, B);
+	int result = abs(-25);
+	printf("The absolute value is = %d", result);
 	
 return 0;
 }
