798712d49cee0ec5cb2ef651461d02b009e587fa
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-17 08:26:06 +0600

    Result_sin()

--- sumon.c
+++ sumon.c
@@ -4,8 +4,8 @@ int main(){
 	double x, result;
 	printf("Enter your number: ");
 	scanf("%lf", &x);
-	result = exp(x);
-	printf("The result of ecplanation(e^x) = %lf", result);
+	result = sin(x);
+	printf("The result of sine = %lf", result);
 	
 
 
