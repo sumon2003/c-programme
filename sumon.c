0a2860a68a1bab299db881b8019dd8b5be535ea3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-18 20:09:52 +0600

    Maximum_number

--- sumon.c
+++ sumon.c
@@ -1,13 +1,17 @@
 #include<stdio.h> 
 #include<math.h>
 int main(){
-	char ch;
-	printf("Enter a character: ");
-	scanf("%c",&ch);
-	if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u')
-	     printf("Vowel");
+	int num1, num2, num3;
+	printf("Enter three number: ");
+	scanf("%d %d %d", &num1, &num2, &num3);
+	if(num1>num2 && num1>num3)
+	  printf("Large number is: %d", num1);
+	else if(num2>num1 && num2>num3)
+	  printf("Large number is: %d", num2);
+	else if(num3>num1 && num3>num2)
+	  printf("Large number is: %d", num3);
 	else
-	     printf("Consonant");	 
+	  printf("Numbers are equal");       
 
 return 0;
 }
