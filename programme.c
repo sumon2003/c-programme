aae7046201e58a62817acfd64d07dc9b21a87026
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-20 08:20:05 +0600

    PrimeNumber_define

--- programme.c
+++ programme.c
@@ -1,12 +1,19 @@
 #include<stdio.h>
 int main(){
- int n, i, factorial=1;
- printf("Enter any positive number: ");
- scanf("%d",&n);
- for(i=1; i<=n; i++)
- {
-    factorial = factorial * i;
- }
- printf("The factorial value is = %d",factorial);
+int num, count=0, i;
+printf("Enter a number: ");
+scanf("%d",&num);
+for(i=2; i<num; i++)
+{
+    if(num%i==0)
+    {
+        count++;
+        break;
+    }
+}
+if(count==0)
+printf("This is a prime number.\n");
+else
+printf("This is not a prime number.");
     return 0;
 }
