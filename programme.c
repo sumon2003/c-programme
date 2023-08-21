997cf4b08644731a71009bd80c4f8e61bfc74637
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-21 10:30:51 +0600

    Greatest Number

--- programme.c
+++ programme.c
@@ -1,16 +1,15 @@
 #include<stdio.h> 
 int main() {
- double A, B, C, a_rec_tri, rad_cir, trapezium, a_square, a_rec;
- scanf("%lf%lf%lf", &A,&B,&C);
-a_rec_tri = 1.0/2*(A*C);
-rad_cir = 3.14159*C*C;
-trapezium = (((A+B)/2)*C);
-a_square = B*B;
-a_rec = A*B;
-printf("TRIANGULO: %.3lf\n",a_rec_tri);
-printf("CIRCULO: %.3lf\n",rad_cir);
-printf("TRAPEZIO: %.3lf\n",trapezium);
-printf("QUADRADO: %.3lf\n",a_square);
-printf("RETANGULO: %.3lf\n",a_rec);
+ int a,b,c;
+ scanf("%d%d%d",&a,&b,&c);
+ if(a>b && a>c)
+ printf("%d eh o maior\n", a);
+ else if(b>a && b>c)
+ printf("%d eh o maior\n", b);
+ else
+ printf("%d eh o maior\n", c);
+
     return 0;
 }
+//MaiorAB = ((a+b+a*b*s*(a-b))/2);
+//printf("%d eh o maior",MaiorAB);
