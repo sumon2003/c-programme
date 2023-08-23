651d14176ebd68a4d697257f70c5d40ff72d5028
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-23 21:42:44 +0600

    Pattern_2

--- programme.c
+++ programme.c
@@ -3,11 +3,11 @@ int main() {
 int n, row, col;
 printf("Enter N = ");
 scanf("%d",&n);
-for(row=1; row<=n; row++)
+for(row=n; row>=1; row--)
 {
     for(col=1; col<=row; col++)
     {
-        printf("%d", row%2);
+        printf("*");
     }
     printf("\n");
 }
