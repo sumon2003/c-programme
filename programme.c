372911b9fe43e100ce638db5cca2c9f9d22ed3ed
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-23 22:19:01 +0600

    Pattern_5

--- programme.c
+++ programme.c
@@ -5,22 +5,16 @@ printf("Enter N = ");
 scanf("%d",&n);
 for(row=1; row<=n; row++)
 {
-    for(col=1; col<=row; col++)
+    for(col=1; col<=n-row; col++)
     {
-        printf("* ");
+        printf(" ");
     }
-    printf("\n");
-}
-for(row=n-1; row>=1; row--)
-{
     for(col=1; col<=row; col++)
     {
-        printf("* ");
+        printf("*");
     }
     printf("\n");
 }
 
-
-
-    return 0;
+  return 0;
 }
