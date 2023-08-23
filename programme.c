cf963c4aa41b3360fa1feb2d9664182adb5392c7
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-23 20:20:23 +0600

    Pattern_1

--- programme.c
+++ programme.c
@@ -1,15 +1,16 @@
 #include<stdio.h> 
 int main() {
- int a,b,c;
- scanf("%d%d%d",&a,&b,&c);
- if(a>b && a>c)
- printf("%d eh o maior\n", a);
- else if(b>a && b>c)
- printf("%d eh o maior\n", b);
- else
- printf("%d eh o maior\n", c);
+int n, row, col;
+printf("Enter N = ");
+scanf("%d",&n);
+for(row=1; row<=n; row++)
+{
+    for(col=1; col<=row; col++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
 
     return 0;
 }
-//MaiorAB = ((a+b+a*b*s*(a-b))/2);
-//printf("%d eh o maior",MaiorAB);
