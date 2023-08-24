70ce4eafc7cdc2016d2f299db42104cebbf22b9f
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-24 06:18:16 +0600

    Pyramid_1

--- programme.c
+++ programme.c
@@ -3,7 +3,7 @@ int main() {
 int n, row, col;
 printf("Enter N = ");
 scanf("%d",&n);
-for(row=n; row>=1; row--)
+for(row=1; row<=n; row++)
 {
     for(col=1; col<=n-row; col++)
     {
@@ -11,7 +11,7 @@ for(row=n; row>=1; row--)
     }
     for(col=1; col<=row; col++)
     {
-        printf("*");
+        printf("* ");
     }
     printf("\n");
 }
