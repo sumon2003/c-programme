20fd495e322fce32cebe6bf90a59b792412aac46
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-24 07:10:51 +0600

    Pyramid_2.1

--- programme.c
+++ programme.c
@@ -3,27 +3,15 @@ int main() {
 int n, row, col;
 printf("Enter N = ");
 scanf("%d",&n);
-for(row=n; row>=1; row--)
-{
-    for(col=1; col<=n-row; col++)
-    {
-        printf(" ");
-    }
-    for(col=1; col<=row; col++)
-    {
-        printf("* ");
-    }
-    printf("\n");
-}
 for(row=1; row<=n; row++)
 {
     for(col=1; col<=n-row; col++)
     {
-        printf(" ");
+        printf("      ");
     }
-    for(col=1; col<=row; col++)
+    for(col=1; col<=2*row-1; col++)
     {
-        printf("* ");
+        printf("sumon ");
     }
     printf("\n");
 }
@@ -31,47 +19,11 @@ for(row=n-1; row>=1; row--)
 {
     for(col=1; col<=n-row; col++)
     {
-        printf(" ");
+        printf("      ");
     }
-    for(col=1; col<=row; col++)
+    for(col=1; col<=2*row-1; col++)
     {
-        printf("* ");
-    }
-    printf("\n");
-}
-for(row=1; row<=n; row++)
-{
-    for(col=1; col<=n-row; col++)
-    {
-        printf(" ");
-    }
-    for(col=1; col<=row; col++)
-    {
-        printf("* ");
-    }
-    printf("\n");
-}
-for(row=n-1; row>=1; row--)
-{
-    for(col=1; col<=n-row; col++)
-    {
-        printf(" ");
-    }
-    for(col=1; col<=row; col++)
-    {
-        printf("* ");
-    }
-    printf("\n");
-}
-for(row=1; row<=n; row++)
-{
-    for(col=1; col<=n-row; col++)
-    {
-        printf(" ");
-    }
-    for(col=1; col<=row; col++)
-    {
-        printf("* ");
+        printf("sumon ");
     }
     printf("\n");
 }
