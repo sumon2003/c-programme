0ae311014f0c808b1801779b8e306fc7c9d6bba0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-24 06:59:37 +0600

    Pyramid_1.3

--- programme.c
+++ programme.c
@@ -3,6 +3,18 @@ int main() {
 int n, row, col;
 printf("Enter N = ");
 scanf("%d",&n);
+for(row=n; row>=1; row--)
+{
+    for(col=1; col<=n-row; col++)
+    {
+        printf(" ");
+    }
+    for(col=1; col<=row; col++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
 for(row=1; row<=n; row++)
 {
     for(col=1; col<=n-row; col++)
@@ -27,6 +39,41 @@ for(row=n-1; row>=1; row--)
     }
     printf("\n");
 }
-
+for(row=1; row<=n; row++)
+{
+    for(col=1; col<=n-row; col++)
+    {
+        printf(" ");
+    }
+    for(col=1; col<=row; col++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
+for(row=n-1; row>=1; row--)
+{
+    for(col=1; col<=n-row; col++)
+    {
+        printf(" ");
+    }
+    for(col=1; col<=row; col++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
+for(row=1; row<=n; row++)
+{
+    for(col=1; col<=n-row; col++)
+    {
+        printf(" ");
+    }
+    for(col=1; col<=row; col++)
+    {
+        printf("* ");
+    }
+    printf("\n");
+}
   return 0;
 }
