b5fd9f69d10bc7ad730dfd6ddf6cddd29993887a
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-28 00:00:21 +0600

    done

--- programme.c
+++ programme.c
@@ -1,17 +1,17 @@
 #include<stdio.h> 
 int main() {
-int num[100],i,n;
+int number[100],i,n;
     printf("How many numbers: ");
     scanf("%d", &n);
     for(i=0; i<n; i++)
     {
-        scanf("%d", &num[i]);
+        scanf("%d", &number[i]);
     }
-   int min = num[0];
+   int min = number[0];
    for(i=1; i<n; i++)
    {
-      if(min>num[i])
-      min = num[i];
+      if(min>number[i])
+      min = number[i];
    }
    printf("Minimum = %d\n", min);
 
