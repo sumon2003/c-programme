c4c62d638a2fe0e196df1f9cc9e047208fdd1d61
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-28 07:11:25 +0600

    car_consumption

--- programme.c
+++ programme.c
@@ -1,10 +1,8 @@
 #include<stdio.h> 
 int main() {
-int distance;
-float fuel_used, avg_consumption;
-scanf("%f %d",&fuel_used,&distance);
-avg_consumption = (fuel_used/distance)*100;
-printf("%.3f km/l\n", avg_consumption); 
-
+float distance;
+float fuel_used;
+scanf("%f %f",&fuel_used,&distance);
+printf("%.3f km/l\n",fuel_used/distance); 
   return 0;
 }
