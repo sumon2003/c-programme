27d1fc3c7228af7e9f30fa670b44fd4f59d49f38
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-29 23:19:50 +0600

    Array_Reverse_Number

--- programme.c
+++ programme.c
@@ -2,10 +2,15 @@
 int main() {
 int i, arr[100], n;
 printf("Enter N = ");
+scanf("%d", &n);
+printf("Enter your numbers: ");
 for(i=0; i<n; i++){
     scanf("%d", &arr[i]);
 }
- printf("%d", arr[i]);
+printf("The reverse number: ");
+for(i=n-1; i>=0; i--){
+    printf("%d ",arr[i]);
+}
 
  return 0;
 }
