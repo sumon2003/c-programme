e8fec4a9590c7459f196c009e7e30ae1c176c6b0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-30 07:13:40 +0600

    Time conversion(Sec to Hour,Min, Sec)

--- programme.c
+++ programme.c
@@ -1,17 +1,12 @@
 #include<stdio.h> 
 int main() {
-int i,n;
-printf("Enter N = ");
-scanf("%d", &n);
-printf("Enter your numbers: ");
-int arr[n];
-for(i=0; i<n; i++){
-    scanf("%d", &arr[i]);
-}
-printf("The reverse number: ");
-for(i=n-1; i>=0; i--){
-    printf("%d ",arr[i]);
-}
+int time, hour, minuite, seconds, remaining_time;
+scanf("%d", &time);
+hour = time/(60*60);
+remaining_time = time%(60*60);
+minuite = remaining_time/60;
+seconds = remaining_time%60;
+printf("%d : %d : %d\n", hour,minuite,seconds);
 
  return 0;
 }
