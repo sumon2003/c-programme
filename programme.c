47d118439a24aaa0832762cbcf8ed0fc38cfa355
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-30 17:56:43 +0600

    Maximum between 3 numbers

--- programme.c
+++ programme.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h> 
 int main(){
-int a,b, max;
+int a,b,c, max, maximum;
 printf("Enter your value: ");
-scanf("%d%d",&a,&b);
+scanf("%d%d%d",&a,&b,&c);
 max = ((a+b)+abs(a-b))/2;
-printf("%d\n", max);
+maximum = ((max+c)+abs(max-c))/2;
+printf("%d eh o maior\n", maximum);
 
  return 0;
 }
