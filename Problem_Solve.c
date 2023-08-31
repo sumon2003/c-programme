9bca104ee8e22434897958c768f8fecbdf9f28da
sumon2003 <sumonkhanbd2003@gmail.com>
2023-08-31 16:30:55 +0600

    Bhashkara's Formula

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
 int main(){
-int A,B,C,D,sum1,sum2;
-scanf("%d%d%d%d",&A,&B,&C,&D);
-sum1=A+B;
-sum2=C+D;
-if(B>C && D>A && sum2>sum1 && C>0 && D>0 && A%2==0){
-    printf("Valores aceitos\n");
+double a,b,c,root,X1,X2;
+scanf("%lf%lf%lf",&a,&b,&c);
+root=sqrt((b*b)-(4*a*c));
+X1=((-b+root)/2*a);
+X2=((-b-root)/2*a);
+if(a==0 || root<=0){
+    printf("Impossivel calcular");
 }else{
-    printf("Valores nao aceitos\n");
+    printf("R1 = %.5lf\n",X1);
+    printf("R2 = %.5lf\n",X2);
 }
+
  return 0;
 }
