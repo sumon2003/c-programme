efbfe949b82e5f59eb1fc0d4390cadedbf1323f3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-06 10:53:56 +0600

    Odd Sum Pair

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main(){
-int T,a,b,i;
+int T,i,a,b,c,sum1,sum2,sum3;
 scanf("%d",&T);
-for(i=0;i<T;i++){ 
-    scanf("%d%d",&a,&b);
-    if(a<10 && b<10)
-    printf("case %d: %d\n",i+1,a+b);   
+for(i=0; i<T; i++){
+   scanf("%d%d%d",&a,&b,&c);
+  sum1 = a+b;
+  sum2 = b+c;
+  sum3 = a+c;
+  if(sum1%2!=0 || sum2%2!=0 || sum3%2!=0)
+   printf("YES\n");
+   else
+   printf("NO\n");
 }
  return 0;
 }
