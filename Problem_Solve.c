03691d5e3c3dd85a3b2cbe1e59244aaf691504b3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-08 19:39:46 +0600

    Theatre Square

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,27 +1,18 @@
 #include<stdio.h>
 int main(){
-float x,y;
-scanf("%f%f",&x,&y);
-if(x>0 && y>0){
-    printf("Q1\n");
-}
-else if(x<0 && y>0){
-    printf("Q2\n");
-}
-else if(x<0 && y<0){
-    printf("Q3\n");
-}
-else if(x>0 && y<0){
-    printf("Q4\n");
-}
-else if(x==0 && y==0){
-    printf("Origem\n");
-}
-else if(x>0 || x<0 && y==0){
-    printf("Eixo X\n");
-}
-else if(x==0 && y<0 || y>0){
-    printf("Eixo Y\n");
-}
+int n,m,a,result1, result2,flagstone;
+float length_side,width_side;
+scanf("%d%d%d",&n,&m,&a);
+length_side =(float)n/a;
+width_side = (float)m/a;
+result1 = ceil(length_side);
+result2 = ceil(width_side);
+flagstone = result1+result2;
+printf("%d\n",result1);
+printf("%d\n",result2);
+printf("%d",flagstone);
+
+
+
  return 0;
 }
