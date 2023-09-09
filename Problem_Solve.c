def1836dfc6f104892e527a52a8868d4f15b8008
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-09 23:52:19 +0600

    Even Numbers

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int main(){
-int s,v1,v2,t1,t2,time1,time2;
-scanf("%d%d%d%d%d",&s,&v1,&v2,&t1,&t2);
-time1 = s * v1 + 2 * t1;
-time2 = s * v2 + 2 * t2;
-if(time1<time2){
-    printf("First\n");
+int num,i,divission;
+scanf("%d",&num);
+for(i=0; i<4; i++){
+    divission = num/10;
 }
-if(time1>time2){
-    printf("Second\n");
+ if(divission%2!=0 && divission<10){
+    printf("ODD");
 }
-if(time1==time2){
-    printf("Friendship\n");
+else if(divission%2==0 && divission>10){
+     printf("EVEN");
 }
- return 0;
+
+   
+return 0;
 }
