5839462bb7e72b8884d3253062d59773599bb0b2
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-12 20:45:11 +0600

    Fixed Passwords

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,26 +1,20 @@
-#include<stdio.h>
-int main(){
-int n,i,p,count_even=0,count_odd=0,count_positive=0,count_negative=0;
-scanf("%d",&n);
-for(i=0; i<n; i++){
-    scanf("%d",&p);
-    if(p%2==0){
-        count_even++;
+#include <stdio.h>
+
+int main() {
+    int n; 
+    scanf("%d", &n); 
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+
+        if (x == 1999) {
+            printf("Correct\n");
+            break;
+        } else {
+            printf("Wrong\n");
+        }
     }
-    if(p%2!=0){
-        count_odd++;
-    }
-    if(p>0){
-        count_positive++;
-    }
-    if(p<0){
-        count_negative++;
-    }
-    
-}
-printf("Even: %d\n",count_even);
-printf("Odd: %d\n",count_odd);
-printf("Positive: %d\n",count_positive);
-printf("Negative: %d\n",count_negative);
-return 0;
+
+    return 0;
 }
