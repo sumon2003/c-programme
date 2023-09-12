5e6ba6ff464609f2da401cfac826a8febd7df861
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-12 17:52:19 +0600

    Count_even_odd_positive-negative

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
 int main(){
-int ar[] = {1,2,4,6,8,10};
-int i,count=0;
-for(i=0; ar[i]!='\0'; i++){
-    count++;
-}    
-    printf("The length of the array is %d\n",count);
-
+int n,i,p,count_even=0,count_odd=0,count_positive=0,count_negative=0;
+scanf("%d",&n);
+for(i=0; i<n; i++){
+    scanf("%d",&p);
+    if(p%2==0){
+        count_even++;
+    }
+    if(p%2!=0){
+        count_odd++;
+    }
+    if(p>0){
+        count_positive++;
+    }
+    if(p<0){
+        count_negative++;
+    }
+    
+}
+printf("Even: %d\n",count_even);
+printf("Odd: %d\n",count_odd);
+printf("Positive: %d\n",count_positive);
+printf("Negative: %d\n",count_negative);
 return 0;
 }
