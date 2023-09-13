609f01db0b0f618e204d34c2ee4d621eb7a477a6
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-13 23:23:30 +0600

    Find Vowel Consonant

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -2,11 +2,14 @@
 int main() {
     char ch;
     scanf("%c",&ch);
-    if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'){
+    if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U' || ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
         printf("%c is a vowel.\n",ch);
     }
+    else if((ch>='A' && ch<='Z') || (ch>='a' && ch<='z')){ //A ---- Z, input= B
+        printf("%c is a Consonant\n",ch);
+    }
     else{
-        printf("%c is not a vowel.\n",ch);
+        printf("%c is not a letter.",ch);
     }
     return 0;
 }
