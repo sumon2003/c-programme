03b70b84aeed82a83e8aac8437608a6fba5f92e0
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-14 00:21:08 +0600

    maximum Array( user input)

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 int main() {
-    int arr[]={12,14,17,11,18};
-    int max=arr[0],i;
-    int min=arr[0];
-    for(i=0; i<5; i++){
-        if(max<arr[i]){  
+    int n,i;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int max = arr[0];
+    for(i=0; i<n; i++){
+        if(max<arr[i]){
             max=arr[i];
-        }  
-        if(min>arr[i]){
-            min=arr[i];
         }
     }
-    printf("%d\n",max);
-    printf("%d\n",min);
-
+    printf("%d",max);
     return 0;
 }
