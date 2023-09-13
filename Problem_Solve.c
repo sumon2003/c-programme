7fde9177d05e2e22e2e59cc34a7a0b319e669c65
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-13 22:51:47 +0600

    Even Odd

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,20 +1,39 @@
 #include <stdio.h>
 int main() {
-    int n,i,value;
-    scanf("%d",&n);
-    for(i=0; i<n; i++) 
-    {
-        scanf("%d",&value);
-        if(value%2==0)
-        {
-            printf("Even\n");
-        }
-        else{
-            printf("ODD\n");
-        }
-
+    int a=5, b=3, c=10, d=6, e=8;
+    if(a%2==0){
+      printf("Even\n");
     }
-   
+      else{
+      printf("Odd\n");
+      }
+    if(b%2==0){
+      printf("Even\n");
+    }
+      else{
+      printf("Odd\n");
+      }  
+    if(c%2==0){
+      printf("Even\n");
+    }
+      else{
+      printf("Odd\n");
+      }  
+    if(d%2==0){
+      printf("Even\n");
+    }
+      else{
+      printf("Odd\n");
+      } 
+    if(e%2==0){
+      printf("Even\n");
+    }
+      else{
+      printf("Odd\n");
+      }         
+    
+
+
    
     return 0;
 }
