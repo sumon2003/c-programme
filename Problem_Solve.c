5cf498fc5fd2247ebab89b4d7066033300e9a41b
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-14 00:48:24 +0600

    Array Sum ( user input)

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 int main() {
-   int arr[]={1,2,3,4,5},sum=0,i;
-   for(i=0;i<5;i++){
-    sum=sum + arr[i];//1+2=3+3=6+4=10+5=15
-   }
-   printf("%d",sum);
+  int n,sum=0,i;
+  scanf("%d",&n);
+  int arr[n];
+  for(i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+    sum=sum+arr[i];
+  }
+  printf("%d",sum);
+
+
+
     return 0;
 }
