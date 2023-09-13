4a60699e4689e6de33e5baa41ff70724eff9a3aa
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-13 23:58:59 +0600

    Maximum Minimum

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -2,13 +2,17 @@
 int main() {
     int arr[]={12,14,17,11,18};
     int max=arr[0],i;
+    int min=arr[0];
     for(i=0; i<5; i++){
         if(max<arr[i]){  
             max=arr[i];
+        }  
+        if(min>arr[i]){
+            min=arr[i];
         }
-       
     }
-    printf("%d",max);
+    printf("%d\n",max);
+    printf("%d\n",min);
 
     return 0;
 }
