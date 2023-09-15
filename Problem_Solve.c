5e7059cc0f7c309c7487506d9b1dabaceae0172a
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-15 22:04:12 +0600

    Max with array

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 int main()
 {
-    int x = 5, y = 10;
-    x += y++;
-    ++x;
-    printf("x = %d, y = %d\n", x, y);
-    --y, x++;
-    printf("x = %d, y = %d\n", x, y--);
-    y %= x++ + 2;
-    printf("x = %d, y = %d\n", x, y);
-    return 0;
+    int arr[15], i;
+    for(i = 0; i < 15; i++)
+        scanf("%d", &arr[i]);
 
+    int mx = arr[0];
+    for(i = 0; i < 15; i++)
+    {
+        if(mx < arr[i])
+        {
+            mx = arr[i];
+        }
+    }
+    printf("%d", mx);
+    return 0;
 }
