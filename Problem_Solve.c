a79c916469930bb063b876fb06e278b52e563a13
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-15 22:25:35 +0600

    Increment Decrement

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -2,12 +2,13 @@
 int main()
 {
     int x = 5, y = 10;
-    x *= --y;
-    ++x;
-    printf("x = %d, y = %d\n", x, y);
+    ++y;
+    printf("x = %d, y = %d\n", x, y*2);
     --y, x++;
-    printf("x = %d, y = %d\n", x, y--);
-    y %= 3 + ++x;
     printf("x = %d, y = %d\n", ++x, y);
+    y %= 10 - ++x;
+    printf("x = %d, y = %d\n", x, y);
+    x %= ++y;
+    printf("x = %d, y = %d\n", x, y--);
     return 0;
 }
