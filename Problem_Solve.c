fb0173772fc8481a02a40d76c8e89c25f207bb44
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-29 15:03:14 +0600

    Next Alphabet

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,14 +1,21 @@
 #include<stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <limits.h>
+#include <stdbool.h>
 int main()
 {
-    int x = 5, y = 10;
-    x += y++;
-    ++x;
-    printf("x = %d, y = %d\n", x, y);
-    --y, x++;
-    printf("x = %d, y = %d\n", x, y--);
-    y %= x++ + 2;
-    printf("x = %d, y = %d\n", x, y);
+    char C;
+    scanf("%c",&C);
+    if(C>='a' && C<'z'){
+        printf("%c\n",C+1);
+    }
+    else if(C>='a' && C<='z'){
+        printf("%c\n",C-25);
+    }
+
     return 0;
 
 }
