0ef4b8dd5cc4979a17c2fcac3bdfda02c3c4429b
sumon2003 <sumonkhanbd2003@gmail.com>
2023-09-29 18:40:47 +0600

    Watermelon

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -7,29 +7,14 @@
 #include <stdbool.h>
 int main()
 {
-    long long a,b,c,d;
-    scanf("%lld %lld %lld %lld",&a,&b,&c,&d);
-    if(a+b-c==d){
-        printf("YES\n");
-    }
-    else if(a-b+c==d){
-        printf("YES\n");
-    }
-    else if(a+b*c==d){
-        printf("YES\n");
-    }
-    else if(a*b+c==d){
-        printf("YES\n");
-    }
-    else if(a*b-c==d){
-        printf("YES\n");
-    }
-    else if(a-b*c==d){
-        printf("YES\n");
-    }
-    else{
-        printf("NO\n");
-    }
+   int w;
+   scanf("%d",&w);
+   if(w%2==0 && w!=2){
+    printf("YES\n");
+   }
+   else{
+    printf("NO\n");
+   }
     return 0;
 
 }
