6d9ccb2141ccf732abfc6320846a598da7440ec7
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-04 15:49:51 +0600

    Theatre Square

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -8,19 +8,14 @@
 #include<stdbool.h>
 int main()
 {
- int n,k,i,count=0;
- scanf("%d %d",&n,&k);
- int arr[n];
- for(i=0; i<n; i++){
-  scanf("%d",&arr[i]);
- }
- for(i=0; i<n; i++){
-  if(arr[i]>=arr[k-1] && arr[i]!=0){
-    count++;
-  }
-  
- }
- printf("%d",count);
+ long long n,m,a,length_ceil,width_ceil,min_num_flagstones;
+ double length,width;
+ scanf("%lld %lld %lld",&n,&m,&a);
+ length = (double)n/a;
+ length_ceil = ceil(length);
+ width = (double)m/a;
+ width_ceil = ceil(width);
+ min_num_flagstones = length_ceil * width_ceil;
 
-
+printf("%lld\n",min_num_flagstones);
 }
