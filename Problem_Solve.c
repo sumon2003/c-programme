934e61581fc38e6d81f432fea79df5918795f50a
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-05 11:21:38 +0600

    Multiplication Table

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -8,18 +8,12 @@
 #include<stdbool.h>
 int main()
 {
-   int N,i;
+   int N,i,multiplication;
    scanf("%d",&N);
-   int arr[N];
-   for(i=0; i<N; i++){
-      scanf("%d",&arr[i]);
+   for(i=1; i<13; i++){
+      multiplication = N * i;
+      printf("%d * %d = %d\n",N,i,multiplication);
    }
-   int max = arr[0];
-   for(i=0; i<N; i++){
-      if(max<arr[i]){
-      max = arr[i];
-      }
-   }
-   printf("%d\n",max);
+
  return 0;
 }
