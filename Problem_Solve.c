cf5a8ee25d60889ac7641e71e609a28daa315f94
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-10 15:30:19 +0600

    Reverse array

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -8,21 +8,25 @@
 #include<stdbool.h>
 int main()
 {
-  int mat[3][3],i,j;
-  for(i=0; i<3; i++){
-    for(j=0; j<3; j++){
-        scanf("%d",&mat[i][j]);
+  int arr[]={10,20,30,40,50,60},i,temp;
+  printf("Original Array\n");
+  for(i=0; i<6; i++){
+    printf("%d ",arr[i]);
+  }  
+  printf("Original Array\n");
+  temp = arr[0];
+    arr[0] = arr[5];
+    arr[5] = temp;
+     temp = arr[1];
+    arr[1] = arr[4];
+    arr[4] = temp;
+     temp = arr[2];
+    arr[2] = arr[3];
+    arr[3] = temp;
+    for(i=0; i<6; i++){
+      printf("%d ",arr[i]);
     }
-    
-  }
-  printf("\n");
 
-  for(i=0; i<3; i++){
-    for(j=0; j<3; j++){
-        printf("%d ",mat[i][j]);
-    }
-    printf("\n");
-  }
   
  return 0;
 }
