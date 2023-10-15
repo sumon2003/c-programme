bfd67b7f8a0221cc67b41b9041eba56fbe04ba14
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-15 22:10:27 +0600

    End Of File

--- programme.c
+++ programme.c
@@ -1,22 +1,13 @@
 #include<stdio.h>
 int main(){
-int n;
-scanf("%d",&n);
-int arr[n],i;
-for(i=0; i<n; i++){
-    scanf("%d",&arr[i]);
-}
-int num,flag=0;
-scanf("%d",&num);
-for(i=0; i<n; i++){
-    if(num==arr[i]){
-        printf("Index is: %d\n",i);
-        flag = 1;
-        break;
-    }    
-}
-if(!flag){
-    printf("%d not found\n",num);
+long long A,B;
+while((scanf("%lld %lld",&A,&B))!=EOF){
+    if(A>B){
+        printf("%lld\n",(A-B));
+    }
+    else{
+        printf("%lld\n",(B-A));
+    }
 }
 return 0;
 }
