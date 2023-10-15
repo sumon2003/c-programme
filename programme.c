b291ab7ae9bad5b3608d95f10bbd8ba6cf702889
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-16 00:22:30 +0600

    Input String using gets Function

--- programme.c
+++ programme.c
@@ -1,20 +1,9 @@
 #include<stdio.h>
 #include<string.h>
 int main(){
-int arr1[9],arr2[9],i,add,first_digit,count = 0,sum;
-for(i=9; i<=0; i--){
-    scanf("%d %d",&arr1[i],&arr2[i]);
-    if(arr1[i]>arr2[i] || arr1[i]<arr2[i]){
-        add = arr1[i]+arr2[i];
-        if(add>10){
-            first_digit = add/10;
-            count++;
-        }
-    }
-    sum = arr1[i] + arr2[i] + first_digit;
-}
-printf("%d",count);
-
-
+char string[100];
+gets(string);
+printf("Hello, World!\n");
+printf("%s\n",string);
 return 0;
 }
