d1b0e05c5c9484ec9a66b68da7ea243ce59d482a
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-15 22:59:11 +0600

    Hazz and Umrah

--- programme.c
+++ programme.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
-long long A,B;
-while((scanf("%lld %lld",&A,&B))!=EOF){
-    if(A>B){
-        printf("%lld\n",(A-B));
+char string[10];
+int count = 1;
+while(1){
+    scanf("%s",string);
+    if(string[0]=='*'){
+        break;
     }
-    else{
-        printf("%lld\n",(B-A));
+    if(string[0]=='H'){
+        printf("Case %d: Hajj-e-Akbar\n",count);
     }
+    else if(string[0]=='U'){
+        printf("Case %d: Hajj-e-Asghar\n",count);
+    }
+    count++;
 }
 return 0;
 }
