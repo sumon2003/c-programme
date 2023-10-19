44c4aa205e05b6bca13e4eadb1169e39d29c2b52
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-19 11:09:18 +0600

    For loop in C

--- programme.c
+++ programme.c
@@ -1,33 +1,24 @@
 #include<stdio.h>
-#include<string.h>
 int main(){
-int year;
-int track = 0;
-while(scanf("%d",&year)!=EOF){
-    int leap_year = 0;
-    int hulukulu = 0;
-    int bulukulu = 0;
+    int a,b,i=0;
+    char* arr[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+    scanf("%d%d",&a,&b);
+    for(i=a;i<=b;i++){
+         if(i>9 && i%2==0)
+            {
+             printf("even\n");
+            }
+         else if(i>9 && i%2)
+       {
+         printf("odd\n");
+       }
+         else
+        {
+         printf("%s\n",arr[i]);
+        }
 
-if((year%4==0 && year%100!=0)||year%400){
-    printf("This is leap year\n");
-    leap_year = 1;
-}
- if(year%15==0){
-    printf("This is huluculu festival year.\n");
-    hulukulu = 1;
-}
- if(year%55==0 && year%400==0){
-    printf("This is bulukulu festival year.\n");
-    bulukulu = 1;
-}
-if(!leap_year && !hulukulu && !bulukulu){
-    printf("This is an ordinary year.\n");
-}
-track++;
-if(track>1){
-    printf("\n");
-}
+    }
 
-}
+
 return 0;
 }
