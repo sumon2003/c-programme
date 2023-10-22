c363ad1749f17143dc788b0f95f5a2b06ced48eb
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-22 20:06:50 +0600

    Stones on the Table

--- programme.c
+++ programme.c
@@ -2,24 +2,18 @@
 #include<string.h>
 #include<ctype.h>
 int main() {
-  char string[101],out_string[101];
-  scanf("%s",string);
-  int length = strlen(string);
-  int i,j=0;
-  for(i=0; i<length; i++)
+  int n,i,count=0;
+  char colour[51];
+  scanf("%d",&n);
+  scanf("%s",colour);
+  for(i=0; i<n; i++)
   {
-   string[i] = tolower(string[i]);
-   if(string[i] != 'a' && string[i] != 'e' && string[i] != 'i' && string[i] != 'o' && string[i] != 'u')
-   {
-     out_string[j] = string[i];
-      j++;
-   }
+    if(colour[i]==colour[i+1])
+    {
+      count++;
+    }
   }
-  int len = strlen(out_string);
-  for(int k=0; k<len; k++)
-  {
-   printf(".%c",out_string[k]);
-  }
+  printf("%d",count);
 
   return 0;
 }
