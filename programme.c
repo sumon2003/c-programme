a0566938cf3faf241778361faa988e2d17814b89
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-22 10:14:34 +0600

    Petiya and two strings strcasecmp()

--- programme.c
+++ programme.c
@@ -1,10 +1,33 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 
 int main() {
-   char ch;
-   printf("Enter the first letter of your name: \n");
-   ch = getchar();
-   printf("The first letter of my name is %c.\n",ch);
+ 
+  char string1[101], string2[101];
+  scanf("%s %s",string1,string2);
+  int len = strlen(string1);
+  int i;
+  for(i=0; i<len; i++){
+    string1[i] = tolower(string1[i]);
+    string2[i] = tolower(string2[i]);
+  }
+  for(i=0; i<len; i++){  
+    if(string1[i]>string2[i]){
+      printf("1\n");
+      return 0;
+    }
+    else if(string1[i]<string2[i]){
+      printf("-1\n");
+      return 0;
+    } 
+  }
+  printf("0\n");
   return 0;
 }
+/* 
+  */
+ /*char string1[101], string2[101];
+ scanf("%s %s",string1,string2);
+ int result = strcasecmp(string1, string2);
+ printf("%d",result);*/
