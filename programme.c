97c9d26bcd036425ca26bb7fd012099a5683db2c
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-22 13:03:06 +0600

    Word Capitalization

--- programme.c
+++ programme.c
@@ -3,24 +3,11 @@
 #include<ctype.h>
 
 int main() {
- char math[101];
- scanf("%s",math);
- int len = strlen(math);
- int i,swap,j;
- for(j=0; j<len; j+=2)
-    {
-        for(i=j+2; i<len; i+=2)
-        {
-            if(math[j]>math[i])
-            {
-                swap = math[j];
-                math[j] = math[i];
-                math[i] = swap;
-            }
-        }
-    }
-    printf("%s\n",math);
-    
+ char word[1001];
+ scanf("%s",word);
+ word[0] = toupper(word[0]);
+ printf("%s",word);
+
   return 0;
 }
 
