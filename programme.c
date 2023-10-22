6a093ec8a1c2249aba11df07a65bf6e302488895
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-22 12:28:06 +0600

    Helpful Maths

--- programme.c
+++ programme.c
@@ -3,31 +3,25 @@
 #include<ctype.h>
 
 int main() {
- 
-  char string1[101], string2[101];
-  scanf("%s %s",string1,string2);
-  int len = strlen(string1);
-  int i;
-  for(i=0; i<len; i++){
-    string1[i] = tolower(string1[i]);
-    string2[i] = tolower(string2[i]);
-  }
-  for(i=0; i<len; i++){  
-    if(string1[i]>string2[i]){
-      printf("1\n");
-      return 0;
+ char math[101];
+ scanf("%s",math);
+ int len = strlen(math);
+ int i,swap,j;
+ for(j=0; j<len; j+=2)
+    {
+        for(i=j+2; i<len; i+=2)
+        {
+            if(math[j]>math[i])
+            {
+                swap = math[j];
+                math[j] = math[i];
+                math[i] = swap;
+            }
+        }
     }
-    else if(string1[i]<string2[i]){
-      printf("-1\n");
-      return 0;
-    } 
-  }
-  printf("0\n");
+    printf("%s\n",math);
+    
   return 0;
 }
-/* 
-  */
- /*char string1[101], string2[101];
- scanf("%s %s",string1,string2);
- int result = strcasecmp(string1, string2);
- printf("%d",result);*/
+
+ 
