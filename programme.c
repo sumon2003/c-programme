73586334eee18800c8249470cd4a6e49dcb9b107
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-23 06:48:03 +0600

    Create A New String

--- programme.c
+++ programme.c
@@ -2,22 +2,13 @@
 #include<string.h>
 #include<ctype.h>
 int main() {
- int k,n,w;
- scanf("%d %d %d",&k,&n,&w);
- int i,total_cost=0,borrowed_Money;
- for(i=1; i<=w; i++)
- {
-  total_cost += k*i;
- }
- if(total_cost<=n)
- {
-  printf("0\n");
- }
- else if(total_cost>n)
- {
-  borrowed_Money = total_cost - n;
-  printf("%d",borrowed_Money);
- }
+ char S[1001],T[1001];
+ scanf("%s",S);
+ scanf("%s",T);
+ int len_S = strlen(S);
+ int len_T = strlen(T);
+ printf("%d %d\n",len_S,len_T);
+ printf("%s %s",S,T);
 
   return 0;
 }
