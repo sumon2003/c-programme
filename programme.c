87e6e6f59d13e9fce600ec47f21c46ddaea7a813
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-23 06:32:59 +0600

    Soldier and Bananas

--- programme.c
+++ programme.c
@@ -2,14 +2,22 @@
 #include<string.h>
 #include<ctype.h>
 int main() {
-  int a,b,count=0;
-  scanf("%d %d",&a,&b);
-  while(a<=b){
-    a=a+a+a;
-    b=b+b;
-    count++;
-  }
-  printf("%d",count);
+ int k,n,w;
+ scanf("%d %d %d",&k,&n,&w);
+ int i,total_cost=0,borrowed_Money;
+ for(i=1; i<=w; i++)
+ {
+  total_cost += k*i;
+ }
+ if(total_cost<=n)
+ {
+  printf("0\n");
+ }
+ else if(total_cost>n)
+ {
+  borrowed_Money = total_cost - n;
+  printf("%d",borrowed_Money);
+ }
 
   return 0;
 }
