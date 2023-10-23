b43059680af56e5c10395a5690695a11e570639f
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-23 16:27:28 +0600

    String Task(Solved)

--- programme.c
+++ programme.c
@@ -3,18 +3,25 @@
 #include<ctype.h>
 #include<math.h>
 int main(){
-char S[1001];
-scanf("%s",S);
-char original_S[1001];
-strcpy(original_S, S);
-
-strrev(S);
-
-if(strcmp(S, original_S) == 0){
-  printf("YES\n");
-}else{
-  printf("NO\n");
-}
+char string[101],out_string[101];
+  scanf("%s",string);
+  int length = strlen(string);
+  int i,j=0;
+  for(i=0; i<length; i++)
+  {
+   string[i] = tolower(string[i]);
+   if(string[i] != 'a' && string[i] != 'e' && string[i] != 'i' && string[i] != 'o' && string[i] != 'u' && string[i] != 'y')
+   {
+     out_string[j] = string[i];
+      j++;
+   }
+  }
+out_string[j]='\0';
+  int len = strlen(out_string);
+  for(int k=0; k<len; k++)
+  {
+   printf(".%c",out_string[k]);
+  }
 
   return 0;
 }
