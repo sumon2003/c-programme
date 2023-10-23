e27efdfeb674159e2219bbb20486d3ffbe6b0771
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-23 10:55:42 +0600

    Count(Inside the string digit)

--- programme.c
+++ programme.c
@@ -3,21 +3,15 @@
 #include<ctype.h>
 #include<math.h>
 int main(){
-char a[11],b[11];
-scanf("%s",a);
-scanf("%s",b);
-
-int len_a = strlen(a);
-int len_b = strlen(b);
-
-printf("%d %d\n",len_a,len_b);
-printf("%s%s\n",a,b);
-
-int swap = a[0];
-    a[0] = b[0];
-    b[0] = swap;
-
-printf("%s %s\n",a,b);
+char S[1000001];
+scanf("%s",S);
+int len_S = strlen(S);
+int i,count=0;
+for(i=0; i<len_S; i++)
+{
+  count += S[i] - '0'; // replace string to ASCII to a string number 
+}                      //Then Count++
+printf("%d\n",count);
 
   return 0;
 }
