b01fee6865889a8ad23f2d3fd6c7a0b156c95155
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-24 09:04:45 +0600

    Anton and Danik

--- programme.c
+++ programme.c
@@ -2,24 +2,30 @@
 #include<string.h>
 #include<ctype.h>
 #include<math.h>
-//USE of sprintf() function//
 int main(){
-long long int num;
-scanf("%lld",&num);
-char string[100];
-sprintf(string, "%lld", num);//Using sprintf() to convert a number into a String.
-int len_string = strlen(string);
-int i,count=0;
-for(i=0; i<len_string; i++){
-  if(string[i]=='7' || string[i]=='4'){
-    count++;
+int n,i;
+scanf("%d",&n);
+char S[100001];
+scanf("%s",S);
+int count_A=0, count_D=0;
+for(i=0; i<n; i++){
+  if(S[i]=='A'){
+    count_A++;
+  }
+  else if(S[i]=='D'){
+    count_D++;
   }
 }
-if(count==7 || count==4){
-  printf("YES");
-}else{
-  printf("NO");
+if(count_A > count_D){
+  printf("Anton");
 }
+else if(count_A < count_D){
+  printf("Danik");
+}
+else if(count_A == count_D){
+  printf("Friendship");
+}
+
   return 0;
 }
 
