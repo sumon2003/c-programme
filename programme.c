fa4d030ee1de50984c2a4b2860ca2fde59945cb7
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-25 08:10:15 +0600

    In Search of an Easy Problem

--- programme.c
+++ programme.c
@@ -3,13 +3,17 @@
 #include<ctype.h>
 #include<math.h>
 int main(){
-int num;
-scanf("%d",&num);
-if(num%4==0 || num%7==0 || num%44==0 || num%77==0 || num%444==0 || num%777==0 || num%47==0 || num%74==0 || num%447==0 || num%774==0 || num%474==0 || num%747==0 || num%477==0 || num%744==0){
-  printf("YES\n");
-}else{
-  printf("NO\n");
+int n;
+scanf("%d",&n);
+int arr[n], i;
+for(i=0; i<n; i++){
+  scanf("%d",&arr[i]);
+    if(arr[i]==1){
+      printf("HARD");
+      return 0;
+    }
 }
+printf("EASY");
     
   return 0;
 }
