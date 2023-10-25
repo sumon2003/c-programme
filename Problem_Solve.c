609bea6a023a681d941d63f5e2be045a3247d1eb
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-25 21:39:51 +0600

    clearScreen() and Sleep()

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,8 +1,18 @@
 #include<stdio.h>
+#include<windows.h>
+void clearScreen() {
+    printf("\033[H\033[J"); // ANSI escape code to clear the screen
+}
 int main()
 {
-  int a,b,difference;
-  difference = b - a;
-  printf("%d\n",difference);
- return 0;
+  
+  for(int j=1; j<=10; j++){
+    printf("%d--->\n",j);
+     for(int i=1; i<=10; i++){
+       clearScreen();
+       printf("%d X %d = %d\n",j,i,j*i);
+       Sleep(1500);
+     }
+  }
 }
+
