9b9df5910f25a712fe0594e884ca54fb06a7eceb
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-25 07:50:41 +0600

    Lucky Divission

--- programme.c
+++ programme.c
@@ -3,22 +3,12 @@
 #include<ctype.h>
 #include<math.h>
 int main(){
-char S[101];
-scanf("%s",S);
-int len_S = strlen(S);
-char word[] = "hello";
-int j=0, count=0, i;
-for(i=0; i<len_S; i++){
-  if(S[i]==word[j]){
-    j++;
-    count++;
-  }
-}
-if(count==5){
-  printf("YES");
-}
-else{
-  printf("NO");
+int num;
+scanf("%d",&num);
+if(num%4==0 || num%7==0 || num%44==0 || num%77==0 || num%444==0 || num%777==0 || num%47==0 || num%74==0 || num%447==0 || num%774==0 || num%474==0 || num%747==0 || num%477==0 || num%744==0){
+  printf("YES\n");
+}else{
+  printf("NO\n");
 }
     
   return 0;
