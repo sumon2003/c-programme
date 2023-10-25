44df3ee459091036810c4c97474a844c90cd457d
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-25 10:03:59 +0600

    George and Accommodation

--- programme.c
+++ programme.c
@@ -5,15 +5,17 @@
 int main(){
 int n;
 scanf("%d",&n);
-int arr[n], i;
+int Pi, Qi, i, count=0;
 for(i=0; i<n; i++){
-  scanf("%d",&arr[i]);
-    if(arr[i]==1){
-      printf("HARD");
-      return 0;
-    }
+  scanf("%d %d",&Pi,&Qi);
+  if(Qi-Pi==1){
+    continue;
+  }
+  else if(Pi<Qi){
+    count++;
+  }
 }
-printf("EASY");
+printf("%d",count);
     
   return 0;
 }
