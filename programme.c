a344df8f12d7fb29670f4314ca67df7e790f4986
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-25 17:10:54 +0600

    STOP WATCH Project

--- programme.c
+++ programme.c
@@ -1,21 +1,38 @@
 #include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<math.h>
-int main(){
-int n;
-scanf("%d",&n);
-int Pi, Qi, i, count=0;
-for(i=0; i<n; i++){
-  scanf("%d %d",&Pi,&Qi);
-  if(Qi-Pi==1){
-    continue;
-  }
-  else if(Pi<Qi){
-    count++;
-  }
+#include<conio.h>
+#include<windows.h>
+
+#define CYCLE 5
+void clearScreen() {
+    printf("\033[H\033[J"); // ANSI escape code to clear the screen
 }
-printf("%d",count);
+int main(){
+    int hour, minute, second;
+    printf("Enter Hour Minute Second:\n");
+    scanf("%d%d%d",&hour, &minute, &second);
+
+    int h=0, m=0, s=0;
+    while(1)
+    {
+      clearScreen();
+         printf("\n\n########      STOP WATCH     ########\n\n");
+         printf("               %.2d:%.2d:%.2d\n\n",h,m,s);
+         printf("#####################################\n");
+         if(h==hour && m==minute && s==second){
+          break;
+         }
+         s++;
+         Sleep(1000);
+         if(s == CYCLE){
+          m++;
+          s=0;
+         }
+         if(m == CYCLE){
+          h++;
+          m=0;
+         }
+    }
+
     
   return 0;
 }
