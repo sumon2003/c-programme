4439c65bea6f2a7cd998e8a449756f833b34dcf3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-26 09:11:06 +0600

    Reversing

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+
 int main()
 {
-  int n;
-  scanf("%d",&n);
-  int arr[n],i;
+  long long int n;
+  scanf("%lld",&n);
+  long long int arr[n],i;
   for(i=0; i<n; i++){
-    scanf("%d",&arr[i]);
-    if(arr[i]>10){
-      continue;
-    }
-    else if(arr[i]<=10){
-      printf("A[%d] = %d\n",i,arr[i]);
-    }
+    scanf("%lld",&arr[i]);
+  }
+  for(i=n-1; i>=0; i--){
+    printf("%lld ",arr[i]);
   }
 
 
