90a49b522d81e8e9c13947306e31b540740f1693
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-26 15:47:01 +0600

    Lucky Array

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -10,15 +10,24 @@ int main()
 {
   int n;
   scanf("%d",&n);
-  int arr[n];
+  int arr[n],count;
   for(int i=0; i<n; i++){
     scanf("%d",&arr[i]);
   }
   qsort(arr, n, sizeof(int), compare);
 
   for(int i=0; i<n; i++){
-    printf("%d ",arr[i]);
+    if(arr[0]==arr[i]){
+      count++;
+    }
   }
+ // printf("%d",count); 
+  if(count%2==0){
+    printf("Unlucky\n");
+  }else{
+    printf("Lucky\n");
+  }
+  
 
  return 0;
 }
