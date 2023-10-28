52e41d5d8aa55b9880360a4ff34d4557747957d8
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-28 07:57:12 +0600

    Calculating Function

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,33 +1,23 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
+#include<math.h>
+/*
 // Comparison function for ascending order
 int compare(const void *a, const void *b) {
     return (*(int *)a - *(int *)b);
 }
-
+*/
 int main()
 {
-  int n;
-  scanf("%d",&n);
-  int arr[n],count;
-  for(int i=0; i<n; i++){
-    scanf("%d",&arr[i]);
-  }
-  qsort(arr, n, sizeof(int), compare);
-
-  for(int i=0; i<n; i++){
-    if(arr[0]==arr[i]){
-      count++;
-    }
-  }
- // printf("%d",count); 
-  if(count%2==0){
-    printf("Unlucky\n");
+  long long int n;
+  scanf("%lld",&n);
+  //Foemula: f(n) = (-1)^n * (n // 2) + (-1)^n * (n % 2)
+  if(n%2==0){
+    printf("%lld",n/2);
   }else{
-    printf("Lucky\n");
+    printf("%lld",-((n/2)+1));
   }
-  
 
  return 0;
 }
