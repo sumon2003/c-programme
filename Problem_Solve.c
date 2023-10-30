3870e46bb7db10debd62d5edb54639692b5cd748
sumon2003 <sumonkhanbd2003@gmail.com>
2023-10-30 07:43:43 +0600

    Divisible Problem

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -2,25 +2,18 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
-/*
-// Comparison function for ascending order
-int compare(const void *a, const void *b) {
-    return (*(int *)a - *(int *)b);
-}
-*/
 int main()
 {
- char string[101];
- scanf("%s",string);
- int len = strlen(string);
- for(int i=0; i<len; i++){
-    if(string[i]=='H' || string[i]=='Q' || string[i]=='9'){
-        printf("YES");
-        return 0;
-    }
- }
- printf("NO");
+ long long int t;
+ scanf("%lld",&t);
+ long long int a,b,i;
+ for(i=0; i<t; i++){
+    scanf("%lld %lld",&a,&b);
+    long long int  count =(b - a % b) % b;
 
+    printf("%lld\n",count); 
+ }
+ 
  return 0;
 }
 
