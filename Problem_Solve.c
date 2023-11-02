da7931c5efefd2266efa1d2d65b4fe2e2ebd22af
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-02 13:23:20 +0600

    Contest Task

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,19 +1,29 @@
 #include<stdio.h>
-#include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 #include<math.h>
-int main()
-{
- long long int t;
- scanf("%lld",&t);
- long long int a,b,i;
- for(i=0; i<t; i++){
-    scanf("%lld %lld",&a,&b);
-    long long int  count =(b - a % b) % b;
-
-    printf("%lld\n",count); 
- }
- 
- return 0;
+void check_word(){
+    char S[101];
+scanf("%s",S);
+int len_S = strlen(S);
+char word[] = "hello";
+int j=0, count=0, i;
+for(i=0; i<len_S; i++){
+  if(S[i]==word[j]){
+    j++;
+    count++;
+  }
+}
+if(count==5){
+  printf("YES");
+}
+else{
+  printf("NO");
+}
 }
 
+int main(){
+
+    check_word();
+  return 0;
+}
