9e6c4be16bc3d8222c1be419542d1512daf85051
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-02 14:35:26 +0600

    Contest Task

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -2,28 +2,22 @@
 #include<string.h>
 #include<ctype.h>
 #include<math.h>
-void check_word(){
-    char S[101];
-scanf("%s",S);
-int len_S = strlen(S);
-char word[] = "hello";
-int j=0, count=0, i;
-for(i=0; i<len_S; i++){
-  if(S[i]==word[j]){
-    j++;
-    count++;
-  }
+void Result(){
+    int t,n,k,p,i,count=0;
+   scanf("%d",&t);
+   for(i=1; i<=t; i++){
+    scanf("%d %d %d",&n,&k,&p);
+    count = (k+p)%n;
+    if(count==0){
+      count = n;
+    }
+    
+    printf("Case %d: %d\n",i,count);
+   }  
+   
 }
-if(count==5){
-  printf("YES");
-}
-else{
-  printf("NO");
-}
-}
-
 int main(){
-
-    check_word();
+   Result();
+    
   return 0;
 }
