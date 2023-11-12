fabcbcb4ff02e314365e385ec956a15d66fd4eec
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-12 18:56:10 +0600

    Check vowel or not

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -6,17 +6,16 @@
 
 
 int main(){
-  //@#$%abc
-  char a;
-  printf("Enter a character: ");
-  scanf("%c",&a);
-  if(a>='A' &&  a<='Z'){
-    printf("%c is alphabet.",a);
-  }
-  else if(a>='a' && a<='z'){
-    printf("%c is alphabet.",a);
-  }else{
-    printf("%c is not an alphabet.",a);
-  }
+  char s; // declare vul silo.
+    printf("Enter a charecter: ");
+    scanf("%c",&s);
+    if(s=='A' || s=='E' || s=='I' || s=='O'||s=='U')
+    printf("%c The charecter is vowel",s); //printf e &s lage na, s dite hoy.format specifier %c likhte hobe.
+    else if(s=='a'|| s=='e'|| s=='i'|| s=='o' || s=='u')//else if er vitore condition dew nai.
+    printf("%c The charecter is vowel",s);
+    else{
+      printf("SORRY!");
+    }
+
   return 0;
 }
