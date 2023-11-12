f7c91f460200689b1e7b1f7fcf91be0beefe309f
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-12 19:30:01 +0600

    YES or YES?

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -6,16 +6,18 @@
 
 
 int main(){
-  char s; // declare vul silo.
-    printf("Enter a charecter: ");
-    scanf("%c",&s);
-    if(s=='A' || s=='E' || s=='I' || s=='O'||s=='U')
-    printf("%c The charecter is vowel",s); //printf e &s lage na, s dite hoy.format specifier %c likhte hobe.
-    else if(s=='a'|| s=='e'|| s=='i'|| s=='o' || s=='u')//else if er vitore condition dew nai.
-    printf("%c The charecter is vowel",s);
-    else{
-      printf("SORRY!");
+  int num,i;
+  scanf("%d",&num);
+  char str[4];
+  
+  for(i=0; i<num; i++){
+    scanf("%s",str);
+    if(strcasecmp(str, "YES")==0){
+      printf("YES\n");
+    }else{
+      printf("NO\n");
     }
+  }
 
   return 0;
 }
