741aace53920eb3b99d5815dc892da99afc29827
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-13 07:21:46 +0600

    Check Even or ODD

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,23 +1,19 @@
+//Check a number is EVEN or ODD 
 #include<stdio.h>
 #include<string.h>
 #include<ctype.h>
 #include<math.h>
 #include<stdlib.h>
-
-
 int main(){
-  int num,i;
-  scanf("%d",&num);
-  char str[4];
-  
-  for(i=0; i<num; i++){
-    scanf("%s",str);
-    if(strcasecmp(str, "YES")==0){
-      printf("YES\n");
-    }else{
-      printf("NO\n");
-    }
+  int num;
+  printf("Enter a number: ");
+  scanf("%d",&num); //for example: 2, 5
+  if(num % 2 == 0)
+  {
+    printf("EVEN");
+  }else{
+    printf("ODD");
   }
-
+  
   return 0;
 }
