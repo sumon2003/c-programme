ea0836bd0770f92082e69948a3048a1bd4bacae3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-16 11:40:41 +0600

    Beautiful Year

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -4,31 +4,31 @@
 #include<math.h>
 #include<stdlib.h>
 int main(){
-  char str[1001],str2[1001]={0};
-  gets(str);
-  int len = strlen(str);
-  int count=0;
-  for(int i=0; i<len; i++){
-    if(str[i]>='a' && str[i]<='z'){
-      str2[count] = str[i];
-      count++;
-    }
-  }
-  int cnt=0;
-  for(int i=0; i<count; i++){
+  int year;
+  scanf("%d",&year);
+  char Year[5];
+  
+  while(1){
+    year++;
+    sprintf(Year, "%d", year);
     int isDuplicate=0;
-    for(int j=i+1; j<count; j++){
-      if(str2[i]==str2[j]){
-        isDuplicate=1;
-        break;
-      }
-    }
+     for(int i=0; i<4; i++){
+       for(int j=i+1; j<4; j++){
+         if(Year[i]==Year[j]){
+           isDuplicate=1;
+           break;
+         }
+       }
+       if(isDuplicate){
+         break;
+       }
+     }
     if(!isDuplicate){
-      cnt++;
+      printf("%d",year);
+      break;
     }
   }
- 
-  printf("%d",cnt);
+
 
   return 0;
 }
