9498b6a89e3719a3870524f585a267d4319ff929
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-16 21:24:59 +0600

    Pointer in C

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -4,31 +4,13 @@
 #include<math.h>
 #include<stdlib.h>
 int main(){
-  int year;
-  scanf("%d",&year);
-  char Year[5];
-  
-  while(1){
-    year++;
-    sprintf(Year, "%d", year);
-    int isDuplicate=0;
-     for(int i=0; i<4; i++){
-       for(int j=i+1; j<4; j++){
-         if(Year[i]==Year[j]){
-           isDuplicate=1;
-           break;
-         }
-       }
-       if(isDuplicate){
-         break;
-       }
-     }
-    if(!isDuplicate){
-      printf("%d",year);
-      break;
-    }
-  }
-
-
+  int x = 5;
+  int *ptr;
+  ptr = &x;
+  printf("Value of x = %d\n",x);
+  printf("Address of x = %p\n",&x);
+  printf("Stored address to ptr of x = %p\n",ptr);
+  printf("Address of ptr = %p\n",&ptr);
+  printf("Value of ptr = %d\n",*ptr);
   return 0;
 }
