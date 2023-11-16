bd9004cc3ae2b35ac1ced9b5f12641ddcf4f5db3
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-16 09:49:58 +0600

    Anton and Letters

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -4,19 +4,31 @@
 #include<math.h>
 #include<stdlib.h>
 int main(){
-  char str[201];
-  scanf("%s",str);
+  char str[1001],str2[1001]={0};
+  gets(str);
   int len = strlen(str);
+  int count=0;
   for(int i=0; i<len; i++){
-  if(strncmp(str+i, "WUB", 3) == 0){
-    i+=2;
-    if(i>2){
-      printf(" ");
+    if(str[i]>='a' && str[i]<='z'){
+      str2[count] = str[i];
+      count++;
     }
-    continue;
   }
-  printf("%c",str[i]);
+  int cnt=0;
+  for(int i=0; i<count; i++){
+    int isDuplicate=0;
+    for(int j=i+1; j<count; j++){
+      if(str2[i]==str2[j]){
+        isDuplicate=1;
+        break;
+      }
+    }
+    if(!isDuplicate){
+      cnt++;
+    }
   }
-  
+ 
+  printf("%d",cnt);
+
   return 0;
 }
