d47a224fdcc4426c4e6a9e56dc11bc6267db254e
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-18 21:34:23 +0600

    Array input and Print

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,19 +1,17 @@
 #include<stdio.h>
-#include<string.h>
-#include<ctype.h>
-#include<math.h>
-#include<stdlib.h>
+//#include<string.h>
+//#include<ctype.h>
+//#include<math.h>
+//#include<stdlib.h>
 int main(){
-  
-  char a;
-  printf("Enter a character: ");
-  scanf("%c",&a);
-  if(a >= '0' && a <= '9'){
-    printf("This is a Digit.");
-  }else{
-    printf("This is not a Digit.");
+  int arr[10],i;
+  for(i=0; i<10; i++){
+    printf("Element - %d: ",i);
+    scanf("%d",&arr[i]);
   }
-  
+  for(i=0; i<10; i++){
+    printf("%d ",arr[i]);
+  }
 
   
   return 0;
