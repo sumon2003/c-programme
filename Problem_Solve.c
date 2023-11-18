075ac714e47f8576702d98813ae9221e8dc61de4
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-18 22:07:35 +0600

    Reverse Array

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -4,14 +4,20 @@
 //#include<math.h>
 //#include<stdlib.h>
 int main(){
-  int arr[10],i;
-  for(i=0; i<10; i++){
-    printf("Element - %d: ",i);
+  int arr[3],i;
+  for(i=0; i<3; i++){
+    printf("element-%d:",i);
     scanf("%d",&arr[i]);
   }
-  for(i=0; i<10; i++){
+  printf("The values store into the array are :\n");
+  for(i=0; i<3; i++){
     printf("%d ",arr[i]);
   }
+  printf("\nThe values store into the array in reverse are :\n");
+  for(i=2; i>=0; i--){
+    printf("%d ",arr[i]);
+  }
+
 
   
   return 0;
