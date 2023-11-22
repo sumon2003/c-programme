9dec9210b5a1e07fab90d4e68eec821b0f3af561
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-22 10:09:15 +0600

    2D-Array[Transposed Matrix]

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -3,40 +3,33 @@
 //#include<ctype.h>
 //#include<math.h>
 //#include<stdlib.h>
+
+void transpose_array(int array[][3], int transposed_matrix[][3])
+{
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      transposed_matrix[j][i] = array[i][j];
+    }
+  }
+}
 void print_matrix(int matrix[][3])
 {
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            printf("%d ",matrix[i][j]);
-        }
-        printf("\n");
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      printf("%d ",matrix[i][j]);
     }
+    printf("\n");
+  }
 }
-void multiply_arrays(int array1[][3], int array2[][3], int product_matrix[][3])
-{
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            int product = 0;
-            for(int k=0; k<3; k++){
-                product += array1[i][k] * array2[k][j];
-            }
-            product_matrix[i][j] = product;
-        }
-    }
-}
-
 int main(){
-  int array1[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-  int array2[][3] = {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}};
-  int product_matrix[3][3];
-  multiply_arrays(array1,array2,product_matrix);
-  printf("Matrix 1:\n");
-  print_matrix(array1);
-  printf("Matrix 2:\n");
-  print_matrix(array2);
-  printf("Product Matrix:\n");
-  print_matrix(product_matrix);
-
+  int array[][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  int transposed_matrix[3][3];
+  transpose_array(array, transposed_matrix);
+  printf("Original Matrix:\n");
+  print_matrix(array);
+  printf("Transose Matrix:\n");
+  print_matrix(transposed_matrix);
+  
 
 
   return 0;
