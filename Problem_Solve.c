53d32b9c9797d92ab81bf1abb0a13eb1743121d9
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-22 07:31:08 +0600

    2D_Array: Find max value

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -3,22 +3,27 @@
 //#include<ctype.h>
 //#include<math.h>
 //#include<stdlib.h>
-int main(){
-  int array_3D[3][3][3] = {
-    {1,2,3},
-    {4,5,6},
-    {7,8,9}
-  };
-
-  for(int i = 0; i < 3; i++){
-    for(int j = 0; j < 3; j++){
-      for(int k = 0; k < 3; k++){
-        printf("%d", array_3D[i][j][k]);
+int find_max(int arr[][3])
+{
+  int max_number = arr[0][0];
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      if(arr[i][j] > max_number){
+        max_number = arr[i][j];
       }
-      printf("\n");
     }
-    printf("\n");
   }
+  return max_number;
+}
+int main(){
+  int array_2D[3][3];
+  for(int i=0; i<3; i++){
+    for(int j=0; j<3; j++){
+      scanf("%d",&array_2D[i][j]);
+    }
+  }
+  int Max_value = find_max(array_2D);
+  printf("Max value: %d",Max_value);
 
   return 0;
 }
