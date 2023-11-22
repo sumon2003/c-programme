f2ecae52def737c2dbf8a0774c10e2f761c356b6
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-22 07:15:24 +0600

    3D Array

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -4,27 +4,21 @@
 //#include<math.h>
 //#include<stdlib.h>
 int main(){
-    int array[3][3]={
-      {1,2,3},
-      {4,5,6},
-      {7,8,9}
-    };
-    int sum = 0;
-    for(int i=0; i<3; i++){
-      for(int j=0; j<3; j++){
-        printf("%d ",array[i][j]);
+  int array_3D[3][3][3] = {
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+  };
+
+  for(int i = 0; i < 3; i++){
+    for(int j = 0; j < 3; j++){
+      for(int k = 0; k < 3; k++){
+        printf("%d", array_3D[i][j][k]);
       }
       printf("\n");
     }
-    printf("Sum of the matrix = ");
-    for(int i=0; i<3; i++){
-      for(int j=0; j<3; j++){
-        sum += array[i][j];
-      }
-    }
-    printf("%d",sum);
-
-
+    printf("\n");
+  }
 
   return 0;
 }
