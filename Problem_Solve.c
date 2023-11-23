c1e8b8542438bbcc44e345a9509dde3d1d02da30
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-23 19:24:57 +0600

    Case Convertor

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,13 +1,16 @@
 #include<stdio.h>
-//#include<string.h>
-//#include<ctype.h>
+#include<string.h>
+#include<ctype.h>
 //#include<math.h>
 //#include<stdlib.h>
 int main(){
- int num;
- scanf("%d",&num);
- int sum = num * (num+1)/2;
- printf("%d\n",sum);
+  char str[101];
+  scanf("%s",str);
+  int len = strlen(str);
+  for(int i=0; i<len; i++){
+   str[i] = tolower(str[i]);
+  }
+  printf("%s",str);
   return 0;
 }
 
