7f0151c07782991ad29788ae3d3b0943f0af4d44
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-23 22:03:49 +0600

    string to lowercase

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -4,23 +4,14 @@
 //#include<math.h>
 //#include<stdlib.h>
 int main(){
- int n;
- scanf("%d",&n);
- int sum=0,digit;
- while(n!=1 && n!=4){
-   sum=0;
-   while(n>0){
-      digit = n%10;
-      sum += digit*digit;
-      n/=10;
-   }
-   n=sum;
- }
- if(n==1){
-   printf("Happy\n");
- }else{
-   printf("Not Happy\n");
- }
+ char str[101]; 
+    scanf("%s", str);
+    for (int i = 0; str[i] != '\0'; i++) {
+        str[i] = tolower(str[i]);
+    }
+    printf("%s\n", str);
+
+
   
   return 0;
 }
