d64ca8a07a8967918739d631f5d5bab2cb639264
sumon2003 <sumonkhanbd2003@gmail.com>
2023-11-27 23:34:29 +0600

    Hello world

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,17 +1,8 @@
 #include <stdio.h>
-int main() {
-    int n,a,b;
-    scanf("%d",&n);
-    int tram=0, capacity=0;
-    for(int i=0; i<n; i++){
-        scanf("%d %d",&a,&b);
-         tram = tram - a + b; 
-         if(tram > capacity){
-            capacity = tram;
-         }  
-    }
-    printf("%d\n",capacity);
-    
 
- return 0;
+int main() {
+   printf("Hello World!");
+
+    return 0;
 }
+
