04b412cd3fef3194a6bb2c4190c66ada24910686
sumon2003 <sumonkhanbd2003@gmail.com>
2023-12-24 20:13:24 +0600

    2D Array

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
-#define print printf
-#define scan scanf
+
 int main() {
-   print("Hello World!");
+    int arr[3][3];
+    printf("Enter the value of the array:\n");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    printf("The 2D array: \n");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
 
     return 0;
 }
-
