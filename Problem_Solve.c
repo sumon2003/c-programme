b9159a9891568aabf637cab4e85456883908b528
sumon2003 <sumonkhanbd2003@gmail.com>
2023-12-24 20:42:56 +0600

    2D array Sum

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
 int main() {
-    int arr[3][3];
-    printf("Enter the value of the array:\n");
+    int arr1[3][3];
+    printf("Enter the value of array 1:\n");
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            scanf("%d",&arr[i][j]);
+            scanf("%d",&arr1[i][j]);
         }
     }
-    printf("The 2D array: \n");
+    int arr2[3][3];
+    printf("Enter the value of array 2:\n");
     for(int i=0; i<3; i++){
         for(int j=0; j<3; j++){
-            printf("%d ",arr[i][j]);
+            scanf("%d",&arr2[i][j]);
+        }
+    }
+    int result_arr[3][3];
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            result_arr[i][j] = arr1[i][j] + arr2[i][j];
+        }
+    }
+    printf("The Sum of 2D array: \n");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<3; j++){
+            printf("%d ",result_arr[i][j]);
         }
         printf("\n");
     }
