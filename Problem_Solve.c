1b01d0a19d52db23a63736e50934ce002d9bddaf
sumon2003 <sumonkhanbd2003@gmail.com>
2023-12-24 21:16:39 +0600

    String user input using scanf, gets, fgets

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int arr1[3][3];
-    printf("Enter the value of array 1:\n");
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            scanf("%d",&arr1[i][j]);
-        }
-    }
-    int arr2[3][3];
-    printf("Enter the value of array 2:\n");
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            scanf("%d",&arr2[i][j]);
-        }
-    }
-    int result_arr[3][3];
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            result_arr[i][j] = arr1[i][j] + arr2[i][j];
-        }
-    }
-    printf("The Sum of 2D array: \n");
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            printf("%d ",result_arr[i][j]);
-        }
-        printf("\n");
-    }
+   //User input using scanf() 
+   char str1[100];
+   scanf("%s",str1);
+   printf("%s\n",str1);
+
+   //User input using gets
+   char str2[100];
+   gets(str2);
+   printf("%s\n",str2);
+
+   //User input using fgets
+   char str3[100];
+   fgets(str3, sizeof(str3), stdin);
+   printf("%s",str3);
 
     return 0;
 }
