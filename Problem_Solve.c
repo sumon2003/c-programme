352b870475aaa12d4bc01fd1179f3596aafd62cc
sumon2003 <sumonkhanbd2003@gmail.com>
2023-12-25 07:53:20 +0600

    use of strrev()

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,17 +1,15 @@
 /*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
-//*/   Md Sumon Hossain Khan   //*/
+/*//   Md Sumon Hossain Khan   //*/
 #include <stdio.h>
 #include <string.h>
 int main() {
 
-   char str1[101] = "sumon";
-   char str2[] = " khan";
+   char str[101] = "Md Sumon Hossain Khan";
    
-   //concatenate str2 to end of str1
-   strcat(str1, str2);
-   
-   printf("%s\n", str1);
-   
+   // Reverse the string using strrev (if available)
+   strrev(str);
+
+   printf("%s", str);
     return 0;
 }
 
