6584bd51b74f50e374007b150a82f0bdb779836c
sumon2003 <sumonkhanbd2003@gmail.com>
2023-12-25 07:47:23 +0600

    Use of strcat()

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -3,18 +3,15 @@
 #include <stdio.h>
 #include <string.h>
 int main() {
-   char str1[] = "kumon";
-   char str2[] = "shan";
-   int result = strcmp(str1, str2);
-   printf("%d\n",result);
-   if(result == 0){
-    printf("The two string are equal.");
-   }else if(result > 0){
-    printf("str1 is lexicografically greater than str2.");
-   }else{
-    printf("str1 is lexicografically less than str2.");
-   }
 
+   char str1[101] = "sumon";
+   char str2[] = " khan";
+   
+   //concatenate str2 to end of str1
+   strcat(str1, str2);
+   
+   printf("%s\n", str1);
+   
     return 0;
 }
 
