f6bf58526fe6830a6e478a1b77987d71fe2e4026
sumon2003 <sumonkhanbd2003@gmail.com>
2023-12-25 08:00:33 +0600

    Use of strcpy()

--- Problem_Solve.c
+++ Problem_Solve.c
@@ -1,15 +1,18 @@
 /*//  بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ  //*/
 /*//   Md Sumon Hossain Khan   //*/
+
 #include <stdio.h>
 #include <string.h>
 int main() {
 
-   char str[101] = "Md Sumon Hossain Khan";
-   
-   // Reverse the string using strrev (if available)
-   strrev(str);
+   char str1[101] = "Md Sumon Hossain Khan";
+   char str2[101];
 
-   printf("%s", str);
+   // Now we will copy str1 to str2;
+   strcpy(str2, str1);
+   
+   printf("The original string: %s\n",str1);
+   printf("The copy string: %s\n",str2);
     return 0;
 }
 
