38d8f0082dc766bcc607372bb07ba34d77b9da39
Md Sumon Hossain Khan <121485281+sumon2003@users.noreply.github.com>
2024-01-07 17:48:41 +0600

    Factorial

---
+++ Reccursion_Factorial.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+//#include<string.h>
+//#include<ctype.h>
+//#include<math.h>
+//#include<stdlib.h>
+int Factorial(int n)
+{
+  if(n==0 || n==1){
+    return 1;
+  }else{
+    return n * Factorial(n-1);
+  }
+}
+
+int main(){
+  int number;
+  scanf("%d",&number);
+  int factorial_result = Factorial(number);
+  printf("%d! = %d",number,factorial_result);
+
+
+  return 0;
+}
